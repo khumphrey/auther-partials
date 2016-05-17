dc34233685333a755b3922c6065474328c05859b
Omri Bernstein <hippiccolo@gmail.com>
2016-05-17 11:46:26 -0400

    Little fixes to starting point

--- browser/app/story/detail/story.detail.controller.js
+++ browser/app/story/detail/story.detail.controller.js
@@ -4,10 +4,6 @@ app.controller('StoryDetailCtrl', function ($scope, story, users) {
   $scope.story = story;
   $scope.users = users;
   $scope.$watch('story', function () {
-    // if ($scope.story.author) {
-    //   $scope.story.author_id = $scope.story.author.id
-    // }
-    console.log($scope.story);
     $scope.story.save();
   }, true);
 });
--- seed.js
+++ seed.js
@@ -69,7 +69,7 @@ function generateUsers () {
   var users = doTimes(numUsers, randUser);
   users.push(User.build({
     name: 'Zeke Nierenberg',
-    photo: 'http://media.licdn.com/media/p/5/005/0ac/184/16505c6.jpg',
+    photo: 'https://learndotresources.s3.amazonaws.com/workshop/55e5c92fe859dc0300619bc8/zeke-astronaut.png',
     phone: '(510) 295-5523',
     email: 'zeke@zeke.zeke',
     password: '123',
@@ -77,7 +77,7 @@ function generateUsers () {
   }));
   users.push(User.build({
     name: 'Omri Bernstein',
-    photo: 'http://i.zemanta.com/278070129_80_80.jpg',
+    photo: 'https://learndotresources.s3.amazonaws.com/workshop/55e5c92fe859dc0300619bc8/sloth.jpg',
     phone: '(781) 854-8854',
     email: 'omri@zeke.zeke',
     password: '123'
