c8190b0c319ce434be83aeefa62f69858d41ef3f
Gabriel Lebec <glebec@gmail.com>
2016-08-30 00:51:55 -0400

    weaken(user model): drop email validation
    Email validation renders XSS harder during the security phase of this workshop.

--- server/api/users/user.model.js
+++ server/api/users/user.model.js
@@ -14,10 +14,7 @@ var User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
+    unique: true
   },
   password: Sequelize.STRING,
   isAdmin: {
