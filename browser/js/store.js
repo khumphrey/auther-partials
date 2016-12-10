422a862639628a41fcb7a76464d668321a52f8e4
Gabriel Lebec <glebec@gmail.com>
2016-12-10 03:48:35 -0500

    feat(redux): dev tools

--- browser/js/store.js
+++ browser/js/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
-import createLogger from 'redux-logger'; 
+import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './redux';
 
 export default createStore(
   rootReducer,
-  applyMiddleware(thunk, createLogger())
+  composeWithDevTools(applyMiddleware(thunk, createLogger()))
 );
