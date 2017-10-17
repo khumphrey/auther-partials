9fa602548dbc834fce34f6d5aa3edd8cbf7513f0
Kate Humphrey <kate@fullstackacademy.com>
2017-10-17 11:24:59 -0400

    #6 - Google Strategy

---
+++ secrets.js
@@ -0,0 +1,2 @@
+process.env.GOOG_CLIENT_ID = "33114094863-hapte04vcohemfo5lb1ojpt02b2bu49k.apps.googleusercontent.com";
+ process.env.GOOG_SECRET = "3T6UDoAQELfy1407YyPwqfKn";
--- server/api/auth/google.js
+++ server/api/auth/google.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
 const passport = require('passport');
-
-/* Routes won't work until we define the google strategy! */
+const User = require('../../db').models.user;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+  		  
+// configuring the strategy (credentials verification callback)
+passport.use(
+  new GoogleStrategy({
+    clientID: process.env.GOOG_CLIENT_ID,
+    clientSecret: process.env.GOOG_SECRET,
+    callbackURL: '/api/auth/google/verify'
+  },
+  function (token, refreshToken, profile, done) {
+    // // workshop started you with this code, challenge was where to go from here:
+    // console.log('---', 'in verification callback', profile, '---');
+    // done();
+    var info = {
+      name: profile.displayName,
+      email: profile.emails[0].value,
+      photo: profile.photos ? profile.photos[0].value : undefined
+    };
+    User.findOrCreate({
+      where: {googleId: profile.id},
+      defaults: info
+    })
+    .spread(user => {
+      done(null, user); // this passes user info on to serializeUser (to be defined!)
+    })
+    .catch(done);
+  })
+);
 
 // Google authentication and login
 router.get('/', passport.authenticate('google', { scope: 'email' }));
